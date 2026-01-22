06fe98d22a19849d8c0bbd9e6a4e74ccdac52754
Bharat Singh <Bharats328@gmail.com>
2026-01-22 21:46:11 +0530

    JS bootstrap cdi link added

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,7 @@ function App() {
           </ProductsProvider>
         </CartProvider>
       </div>
+      <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     </>
   );
 }
